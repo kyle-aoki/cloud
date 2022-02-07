72ed8d4e4928045786712164b484259cb59332ed
kyle <kyle.aoki.500@gmail.com>
2022-02-06 22:48:07 -0800

    a

--- pkg/command/create.go
+++ pkg/command/create.go
@@ -11,21 +11,26 @@ import (
 
 func Create() {
 	launchTemp := args.Poll()
-	instanceName := args.Poll()
+	instanceNames := args.Collect()
 	client := amazon.EC2Client()
 
-	rio, err := client.RunInstances(&ec2.RunInstancesInput{
-		MinCount: util.IntToInt64Ptr(1),
-		MaxCount: util.IntToInt64Ptr(1),
-		LaunchTemplate: &ec2.LaunchTemplateSpecification{
-			LaunchTemplateName: util.StrPtr(launchTemp),
-		},
-		TagSpecifications: []*ec2.TagSpecification{{
-			ResourceType: util.StrPtr("instance"),
-			Tags:         []*ec2.Tag{{Key: util.StrPtr("Name"), Value: util.StrPtr(instanceName)}},
-		}},
-	})
-	util.MustExec(err)
+	for i := range instanceNames {
+		rio, err := client.RunInstances(&ec2.RunInstancesInput{
+			MinCount: util.IntToInt64Ptr(1),
+			MaxCount: util.IntToInt64Ptr(1),
+			LaunchTemplate: &ec2.LaunchTemplateSpecification{
+				LaunchTemplateName: util.StrPtr(launchTemp),
+			},
+			TagSpecifications: []*ec2.TagSpecification{{
+				ResourceType: util.StrPtr("instance"),
+				Tags: []*ec2.Tag{{
+					Key:   util.StrPtr("Name"),
+					Value: util.StrPtr(instanceNames[i]),
+				}},
+			}},
+		})
+		util.MustExec(err)
 
-	fmt.Println(*rio.Instances[0].InstanceId)
+		fmt.Println(*rio.Instances[0].InstanceId)
+	}
 }
